fef0bc37e57ddc3c17b96bdf601e829bbe18d004
Jesus <jesupaleta@gmail.com>
2020-10-08 11:50:32 -0500

    Agregado codigo de consulta

--- api.py
+++ api.py
@@ -1,10 +1,57 @@
 from flask import Flask
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+from flask import request, redirect, url_for
+from db import Dbase
+from flask import render_template, session
+import json
+
+dba = Dbase()
 
 @app.route('/')
 def index():
     return 'Index Page'
 
-@app.route('/clients/<client>')
-def get_info(client):
-    return 'Hello, World %s' % (client)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        us = {
+            'usuario': request.form['user'],
+            'auth': request.form['pass']
+        }
+
+        user = dba.get_user(us)
+        session['perfil'] = user['perfil']
+        print('us', user)
+        if us:
+            return redirect(url_for('consult'))
+
+        else:
+            return 'El usuario no existe'
+
+
+
+    else:
+        return render_template('login.html')
+
+
+@app.route('/consult/', methods=['GET', 'POST'])
+def consult():
+    if request.method == 'POST':
+        perfil = session['perfil']
+        cliente = request.form.get('cliente')
+
+        if not cliente:
+            return render_template('consult.html')
+
+        cl = dba.get_client(cliente)
+        print('cl', cl)
+
+        return json.dumps(cl)
+    else:
+        return render_template('consult.html')
+
+
+# @app.route('/clients/<client>')
+# def get_info(client):
+#     return 'Hello, World %s' % (client)
--- db.py
+++ db.py
@@ -1,23 +1,56 @@
-from pymongo import MongoClient
-import csv
+#from pymongo import MongoClient
+import csv, json
+from firebase import firebase
+
 
 class Dbase(object):
     def __init__(self):
-        self.client = MongoClient('localhost', 27017)
-        self.db = client['bbva']
+        self.client = None#MongoClient('mongodb+srv://bbvatest:bbvatest@cluster0.uvfsh.mongodb.net/<dbname>?retryWrites=true&w=majority')
+        #self.db = self.client['bbva']
+        #self.clientes = self.db['clientes']
+        #self.usuarios = self.db['usuarios']
+        self.fb = firebase.FirebaseApplication('https://bbva-a1ba8.firebaseio.com', None)
 
+        self.rst = {
+        'idCliente': 0, 'nombre': 0, 'apellidoPaterno': 1, 'apellidoMaterno': 1,
+        'fechaNacimiento': 1, 'sexo': 0,
+        'segmento': 0,
+        'nacionalidad': 1, 'rfc': 1,
+        'tipoID': 1, 'numeroID': 1, 'cuenta': 0, 'email': 1
+        }
+
+    def get_client(self, cliente):
+        #return self.clientes.find_one({"idCliente": cliente})
+        return self.fb.get('/users/1', None, {'idCliente': cliente})
+
+    def get_user(self, user):
+        with open('baseUsuarios.csv', 'r', encoding = 'utf-8') as f:
+            for lin in csv.DictReader(f):
+                #print('user', lin)
+                if lin['\ufeffusuario'] == user['usuario'] and lin['auth'] == lin['auth']:
+                    return lin
+                return {}
 
     def load_db_clients(self):
-        clientes = db['clientes']
-        with open('baseClientesHackaton8Oct', 'r') as f:
-            for lin in csv.DictReader(f):
-                clintes.insert_one(lin)
 
-    def load_db_users(self):
-        clientes = db['clientes']
-        with open('baseUsuarios', 'r') as f:
+        with open('baseClientesHackaton8Oct.csv', 'r', encoding = 'utf-8') as f:
             for lin in csv.DictReader(f):
-                clintes.insert_one(lin)
+                #lin = {x: lin[x].encode('utf-8') for x in lin}
+                #print('lin', lin)
+                #self.clientes.insert_one(lin)
+                yield lin
 
-dbc = Dbase()
-dbc.load_db_clients()
+
+    def add_json(self):
+        with open('clientes.json', 'w') as f:
+            json.dump(list(self.load_db_clients()), f)
+
+    #def load_db_users(self):
+    #    clientes = db['usuarios']
+    #    with open('baseUsuarios.csv', 'r') as f:
+    #        for lin in csv.DictReader(f):
+    #            self.usuarios.insert_one(lin)
+
+if __name__ == "__main__":
+    dbc = Dbase()
+    dbc.add_json()
